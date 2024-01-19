1cdb7735ee1d19993fb8e273b3882af105fd4300
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-19 21:48:27 +0600

    page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./Product";
+import HomePage from "./HomePage";
+import Pricing from "./Pricing";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
