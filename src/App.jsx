d52b978f126a105e1845a795ef25a525f9b066c9
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-19 21:32:56 +0600

    react router created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./Product";
 
 function App() {
   return (
-    <>
-      <h1>World Wise</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
