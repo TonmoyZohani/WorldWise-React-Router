46f33783a6dcdacb7164aeaf7a6f87ca29d6b053
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-20 22:37:37 +0600

    add index css file

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './AppNav.module.css'
 
 const AppNav = () => {
   return (
-    <div>AppNav</div>
+    <nav className={styles.nav}>AppNav</nav>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PageNav from "../components/PageNav";
 import { Link } from "react-router-dom";
+import AppNav from "../components/AppNav";
 
 const HomePage = () => {
   return (
     <div>
       <PageNav />
-      <div>WorldWise</div>
+      <AppNav/>
+      <h1>WorldWise</h1>
 
       <Link to='/app'>Go to the App</Link>
     </div>
