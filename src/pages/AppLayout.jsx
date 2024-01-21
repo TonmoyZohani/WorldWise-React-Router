7cc21715e79520a3f153a3a6db4348cbb6beeebc
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-21 22:28:51 +0600

    created sidebar css

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import AppNav from "../components/AppNav";
 import Sidebar from "../components/Sidebar";
+import styles from './AppLayout.module.css';
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className={styles.app}>
       <Sidebar />
     </div>
   );
