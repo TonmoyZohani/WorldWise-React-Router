10b9d2b49631a2b9cd048390d439aed91a832cce
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-21 22:45:23 +0600

    created nested route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayOut";
 import Login from "./pages/Login";
 
-
 function App() {
   return (
     <BrowserRouter>
@@ -14,7 +13,11 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>List of Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
