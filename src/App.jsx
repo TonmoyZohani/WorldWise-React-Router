61f5da45c2a12ee81dd1a398f0aada88124e9838
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-22 20:11:57 +0600

    cities json set up

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayOut";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of Cities</p>} />
-          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>List of Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './CityList.module.css';
+
+const CityList = () => {
+  return (
+    <div className={styles.cityList}>CityList</div>
+  )
+}
+
+export default CityList
