ca8202931a1b8a6638024465ac237206ac4839ff
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-22 20:34:34 +0600

    fetch the cities

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,21 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayOut";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cities, setCities] = useState([]);
+
+  const url = `http://localhost:8000/cities`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCities(data));
+  }, []);
+
+  console.log(cities);
+
   return (
     <BrowserRouter>
       <Routes>
