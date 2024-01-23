590a98b95e6eac02ec8d10707ae3bd29aa5153bf
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-23 22:12:12 +0600

    get lat lng by using useSearchParams

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,22 @@
-import styles from './Map.module.css'
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 const Map = () => {
-  return (
-    <div className={styles.mapContainer}>Map</div>
-  )
-}
+  const [searchParams, setSearchParams] = useSearchParams();
 
-export default Map
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  console.log(lat);
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Positions : {lat},{lng}
+      </h1>
+    </div>
+  );
+};
+
+export default Map;
