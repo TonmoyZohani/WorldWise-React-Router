d0ddfa5b3ad7b2dd2bf61e4075f22ea8b8311c30
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-23 21:48:46 +0600

    country link added

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
 
 const CityItem = ({ city }) => {
   console.log(city);
 
+  const {id} = city;
+
   return (
     <li className={styles.cityItem}>
-      <span className={styles.emoji}>{city.emoji}</span>
-      <h3 className={styles.name}>{city.cityName}</h3>
-      <time className={styles.date}>{city.date}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+      <Link to={`${id}`}>
+        <span className={styles.emoji}>{city.emoji}</span>
+        <h3 className={styles.name}>{city.cityName}</h3>
+        <time className={styles.date}>{city.date}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 };
