1cd02aecd48c050306f633eaa92fe87e774b272e
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-23 22:06:42 +0600

    set the query

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const CityItem = ({ city }) => {
   console.log(city);
 
-  const {id} = city;
+  const { id, position } = city;
 
   return (
     <li className={styles.cityItem}>
-      <Link to={`${id}`}>
+      <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span className={styles.emoji}>{city.emoji}</span>
         <h3 className={styles.name}>{city.cityName}</h3>
         <time className={styles.date}>{city.date}</time>
