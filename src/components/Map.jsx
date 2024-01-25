8d1ab5fe15a386ffe997951f7ad53e99de58bd22
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-25 21:33:08 +0600

    route set

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,8 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 const Map = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
@@ -10,7 +11,7 @@ const Map = () => {
   console.log(lat);
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h1>
         Positions : {lat},{lng}
