94f14bf8d71478931a71503e781dd47808eaa62e
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-25 21:24:25 +0600

    set form

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -34,7 +35,7 @@ function App() {
           <Route path="cities" element={<CityList cities={cities} />} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} />} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
