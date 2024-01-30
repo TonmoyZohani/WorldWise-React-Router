3f054408ac35b64db09e1052a35d0811ab50d8f2
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-30 21:29:27 +0600

    map shown

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,9 +2,11 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesProvider";
 
 const Map = () => {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [searchParams, setSearchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([25, 40]);
 
@@ -18,12 +20,12 @@ const Map = () => {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={mapPosition}>
           <Popup>
